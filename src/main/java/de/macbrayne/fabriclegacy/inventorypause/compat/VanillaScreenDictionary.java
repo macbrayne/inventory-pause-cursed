71964fc06e114ab0891e6bc1f287b9312b7cc64c
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-07-25 14:50:07 +0200

    add support for the rest of the implemented screens

--- src/main/java/de/macbrayne/fabriclegacy/inventorypause/compat/VanillaScreenDictionary.java
+++ src/main/java/de/macbrayne/fabriclegacy/inventorypause/compat/VanillaScreenDictionary.java
@@ -12,15 +12,16 @@ import java.util.function.BooleanSupplier;
 
 public class VanillaScreenDictionary {
     private final Class<?>[] vanillaClasses;
-    private final Map<Class<?>, BooleanSupplier> configProviderMap = new HashMap<>(14);
+    private final Map<Class<? extends ContainerScreen>, BooleanSupplier> configProviderMap = new HashMap<>(14);
 
     public VanillaScreenDictionary() {
         // Abilities Screen top layer
-        configProviderMap.put(PlayerInventoryScreen.class, () -> true);
         configProviderMap.put(CraftingScreen.class, () -> true);
-        configProviderMap.put(ChestContainer.class, () -> true);
-
         configProviderMap.put(DispenserScreen.class, () -> true);
+        configProviderMap.put(DoubleChestScreen.class, () -> true);
+        configProviderMap.put(FurnaceScreen.class, () -> true);
+        configProviderMap.put(PlayerInventoryScreen.class, () -> true);
+
         // Cache keySet to improve performance
         vanillaClasses = configProviderMap.keySet().toArray(new Class[0]);
     }
